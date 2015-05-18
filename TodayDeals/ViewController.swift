404046d312a632579b51a22cff0b0976724ca0eb
System Administrator <root@Guilhermes-MacBook-Pro-2.local>
2015-05-18 16:51:37 -0300

    Initial commit from local git

---
+++ TodayDeals/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TodayDeals
+//
+//  Created by Guilherme Leite Colares on 5/18/15.
+//  Copyright (c) 2015 triApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
